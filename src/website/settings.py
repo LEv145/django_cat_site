586449aad3f1975be15f260b8d036ab30220713e
lev <levbezrukov@bk.ru>
2022-05-26 15:26:18 +0200

    Architecture update

--- src/website/settings.py
+++ src/website/settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,6 +14,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "api_app",
 ]
 
 MIDDLEWARE = [
